86bd67304a429d145c627278d0325a631fda3b84
Parag-dwn <79037459+Parag-dwn@users.noreply.github.com>
2022-12-14 21:05:29 +0530

    Co-authored-by: AdityaBopche25 <AdityaBopche25@users.noreply.github.com>

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/base')
+def base():
+    return render_template('base.html')
 @app.route('/about')
 def about():
     return render_template('about.html')   
